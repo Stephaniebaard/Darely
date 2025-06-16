41f85d51e6d9d4c6cd847932d1ac14a9a1417436
Stephanie Baard <stephanie.baard@gmail.com>
2025-06-16 16:44:00 +0200

    Fixed errors with  navigation

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './source/HomeScreen';
-import Game1Screen from './source/WouldYouRather';
+import Game1Screen from './source/WouldYouRather'; console.log('Game1Screen från import:', Game1Screen);
 import Game2Screen from './source/TruthOrDare';
 import Game3Screen from './source/NeverHaveIEver';
 import Game4Screen from './source/Paranoia';
-
 
 export type RootStackParamList = {
   Home: undefined;
@@ -19,51 +18,26 @@ export type RootStackParamList = {
   Game4: undefined;
 };
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export default function Home() {
+export default function App() {
   return (
-    <ImageBackground
-         source={require('./assets/confetti.png')} 
-      style={styles.background}
-      resizeMode="cover" 
-    >
-    <View style={styles.purpleBox}>
-      <Text style={styles.text}>Open up !</Text>
-      <StatusBar style="auto" />
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Would you rather" component={Game1Screen} />
-        <Stack.Screen name="Truth or dare" component={Game2Screen} />
-        <Stack.Screen name="Never have I ever" component={Game3Screen} />
-        <Stack.Screen name="Paranoia" component={Game4Screen} />
+        <Stack.Screen name="Game1" component={Game1Screen} />
+        <Stack.Screen name="Game2" component={Game2Screen} />
+        <Stack.Screen name="Game3" component={Game3Screen} />
+        <Stack.Screen name="Game4" component={Game4Screen} />
       </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-    </ImageBackground>
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
 
+
 const styles = StyleSheet.create({
   background: {
-    flex: 1,            
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  purpleBox: {
-    backgroundColor: 'rgba(134, 97, 172, 1)', 
-    padding: 20,
-    borderRadius:10,
-    borderWidth: 10,                            
-    borderColor: 'rgba(94, 65, 123, 1)', 
-    width: '80%',      
-    height: '80%',      
-    justifyContent: 'center',  
-    alignItems: 'center', 
-  },
-  text: {
-    color: 'rgba(213, 172, 253, 1)',
-    fontSize: 20,
+    flex: 1,
   },
 });
